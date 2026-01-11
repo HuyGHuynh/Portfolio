81297dc64009ad69c41804ec6e7edb034225785f
HuyGHuynh <yuhhuynh@gmail.com>
2026-01-10 20:46:55 -0500

    Add hamburger icon for navbar toggle on phone device

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,60 @@
+import { useState } from "react";
+
 export default function Navbar() {
-    return <nav className="fixed top-0 left-0 w-full z-50 bg-black/5 backdrop-blur-sm overflow-hidden flex items-center justify-between px-4">
-        <a href="#hero" className="font-bold text-lg">Huy</a>
-        <div className="flex gap-6 p-4">
+  const [open, setOpen] = useState(false);
+  console.log("Menu open: ", open);
+
+  return (
+    <>
+      <nav className="fixed top-0 left-0 w-full z-50 bg-black/5 backdrop-blur-sm">
+        <div className=" flex items-center justify-between h-14 px-4 gap-6 p-4">
+        <a href="#hero" className="font-bold text-lg">
+          Huy
+        </a>
+        <div className="hidden md:flex gap-6 p-4">
+          <a href="#about">About</a>
+          <a href="#skill">Skill</a>
+          <a href="#project">Projects</a>
+          <a href="#experience">Experience</a>
+          <a href="#contact">Contact</a>
+          <a href="#resume">Resume</a>
+        </div>
+
+        {/*This is for mobile development */}
+        <button
+          className="md:hidden cursor-pointer"
+          onClick={() => setOpen(!open)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-menu-icon lucide-menu"
+          >
+            <path d="M4 5h16" />
+            <path d="M4 12h16" />
+            <path d="M4 19h16" />
+          </svg>
+        </button>
+        </div>
+
+        {open && (
+          <div className="md:hidden bg-gray-900 text-gray-200 backdrop-blur-sm absolute flex flex-col top-14 left-0 w-full gap-6 p-4">
             <a href="#about">About</a>
             <a href="#skill">Skill</a>
             <a href="#project">Projects</a>
             <a href="#experience">Experience</a>
             <a href="#contact">Contact</a>
             <a href="#resume">Resume</a>
-        </div>
-    </nav>
+          </div>
+        )}
+      </nav>
+    </>
+  );
 }
