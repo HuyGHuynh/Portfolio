435764a6ce490961f1cd8af9d93b7041e44ed6dd
HuyGHuynh <yuhhuynh@gmail.com>
2026-01-10 22:23:01 -0500

    Create a Card components that hold icons and label

---
+++ src/components/Skill.jsx
@@ -0,0 +1,8 @@
+import Card from "./Card";
+import JavaLogo from "../assets/icons/Java.svg";
+
+export default function Skill() {
+  return <section>
+    <Card icon={JavaLogo} label="Java"/>
+  </section>;
+}
