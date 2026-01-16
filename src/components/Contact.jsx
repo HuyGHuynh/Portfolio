256b5fb509be3e7c81d6e05fb07b1cd0af143736
HuyGHuynh <yuhhuynh@gmail.com>
2026-01-16 16:41:46 -0500

    reorder social button

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,24 +16,6 @@ export default function Contact() {
           Send an Email
         </a>
         <div className="flex justify-center gap-6 mt-10">
-        <a href="mailto:yuhhuynh@gmail.com">
-          <SocialButton>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
-              <rect x="2" y="4" width="20" height="16" rx="2" />
-            </svg>
-          </SocialButton>
-        </a>
         <a href="https://github.com/HuyGHuynh" target="_blank" rel="noopener noreferrer">
           <SocialButton>
             <svg
@@ -68,6 +50,24 @@ export default function Contact() {
               <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
               <rect width="4" height="12" x="2" y="9" />
               <circle cx="4" cy="4" r="2" />
+            </svg>
+          </SocialButton>
+        </a>
+        <a href="mailto:yuhhuynh@gmail.com">
+          <SocialButton>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m22 7-8.991 5.727a2 2 0 0 1-2.009 0L2 7" />
+              <rect x="2" y="4" width="20" height="16" rx="2" />
             </svg>
           </SocialButton>
         </a>
